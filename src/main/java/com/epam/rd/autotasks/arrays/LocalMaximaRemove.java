0694a322419a51bdf7c69173d7704a7d3b751118
Nastassia Stanchyk <stanchikaf@gmail.com>
2022-04-08 15:55:26 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
+++ src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
@@ -6,14 +6,30 @@ public class LocalMaximaRemove {
 
     public static void main(String[] args) {
         int[] array = new int[]{18, 1, 3, 6, 7, -5};
-
         System.out.println(Arrays.toString(removeLocalMaxima(array)));
     }
 
     public static int[] removeLocalMaxima(int[] array){
-
         //put your code here
-
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException(); //заглушка
+        int[] copyArray = new int[array.length];
+        int[] myArray;
+        int flag = 0;
+        if (array[0] <= array[1]){
+            System.arraycopy(array, 0, copyArray, flag, 1);
+            flag++;
+        }
+        for (int i = 1; i < array.length - 1; i++) {
+            if ((array[i] <= array[i - 1]) || (array[i] <= array[i + 1])) {
+                System.arraycopy(array, i, copyArray, flag, 1);
+                flag++;
+            }
+        }
+        if (array[array.length - 1] <= array[array.length - 2]) {
+            System.arraycopy(array, array.length - 1, copyArray, flag, 1);
+            flag++;
+        }
+        myArray = Arrays.copyOf(copyArray, flag);
+        return myArray;
     }
 }
